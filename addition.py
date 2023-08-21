0023a68c56fade32481e906483eee60d6d317054
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-08-21 19:07:39 +0530

    addition of two numbers

--- addition.py
+++ addition.py
@@ -1,5 +1,5 @@
 # The code for adding two numbers
-a=input("Enter first number:")
-b=input("Enter second number:")
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
 sum = a + b
 print("The sum of ",a,"and",b,"is",sum)
