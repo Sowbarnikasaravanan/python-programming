32f19e03a2d223bbc1a481b793b03623a5436a46
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-08-23 23:56:07 +0530

    Division program

---
+++ Division.py
@@ -0,0 +1,6 @@
+# To find the quotient and remainder
+num = int(input("Enter the Dividend:"))
+div = int(input("Enter the divisor :"))
+quotient = num // div 
+remainder =num % div 
+print("The quotient and remainder when ",num," is divided by ",div,"are ",quotient,"and",remainder)
