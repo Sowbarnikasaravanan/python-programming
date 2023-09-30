45731b78c029096bcaa6cd62b2293feac4309688
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-09-30 21:52:31 +0530

    Swapping between the  numbers

---
+++ Swap.py
@@ -0,0 +1,9 @@
+#To swap the values between the variables
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
+print("Before swapping :",a,b)
+temp=a
+a=b
+b=temp
+print("After swapping :",a,b)
+      
