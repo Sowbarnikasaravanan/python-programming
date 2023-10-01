020bde20ac5b692dfb85d86d472f7601cb598ace
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-01 21:50:31 +0530

    Converting from celsius to fahrenheit and fahrenheit to celsius

---
+++ Celsiusfahrenheit.py
@@ -0,0 +1,9 @@
+#To calculate celsius to Fahrenheit 
+cel=float(input("Enter the celsius value in degree :"))
+fah=(cel*1.8)+32
+print(cel," degree celsius is equal to ",fah," degree fahrenheit)
+
+#To calculate fahrenheit to celsius
+fah=float(input("Enter the fahrenheit value :"))
+cel=(fah-32)/1.8
+print(fah," degree fahrenheit is equal to ",cel," degree celsius")
