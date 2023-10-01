be068916f0f7dfe461531828db42c795ddf389ed
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-01 22:05:31 +0530

    To check the given year is a leap year or not

---
+++ Leapyearornot.py
@@ -0,0 +1,10 @@
+#To check whether the given year is leap year or not
+year=int(input("Enter the year:"))
+if year%400==0:
+    print(year," is a leap year")
+elif year%100==0:
+    print(year," is not a leap year ")
+elif year%4==0:
+    print(year," is a leap year")
+else:
+    print(year," is not a leap year")
