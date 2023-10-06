1fe6403ea4522ed264b52f5d1b88ae52bc001ff3
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-06 17:07:46 +0530

    To check ASCII value of a character and to check the equivalent character for an integer

---
+++ Asciival.py
@@ -0,0 +1,7 @@
+#To find the equivalent ASCII value for an alphabet
+ch=input("Enter a character:")
+print("The eqivalent ASCII value of ",ch," is ",ord(ch))
+#In python ord() can be used to display an integer value value of a character
+ch=int(input("Enter a positive integer value:"))
+print("The equivalent character of ",ch," is ",chr(ch))
+#chr() function is used to display the character of an integer value
