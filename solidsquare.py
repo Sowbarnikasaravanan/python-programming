9d76bce5e5e5e52f82802a18070cd75313d3b0f9
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-10-27 14:41:25 +0530

    To diplay the solid pattern

---
+++ solidsquare.py
@@ -0,0 +1,6 @@
+# To display the patterns in the form of solid square
+rows=int(input("Enter the size of an array:");
+for i in range(0,rows):
+  for j in range(0,rows):
+    print("*" ,end=" ");
+  print("\n")
