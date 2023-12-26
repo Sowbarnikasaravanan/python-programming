a7284aa1bbef3e980a03c387d369461c6eb6099a
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2023-12-26 13:43:36 +0530

    To display the multiplication table

--- Multtable.py
+++ Multtable.py
@@ -3,4 +3,14 @@ table=int(input("Enter the table:"))
 limit=int(input("Enter the limit:"))
 for i in range(1,limit+1):
     print(i,"*",table,"=",i*table)
-    
+
+'''
+Enter the table:5
+Enter the limit:5
+1*5=5
+2*5=10
+3*5=15
+4*5=20
+5*5=25
+
+'''
