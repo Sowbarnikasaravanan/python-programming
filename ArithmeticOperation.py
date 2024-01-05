46171706fce5da319ed26d123cb37ac557c16de9
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-05 20:44:09 +0530

    Example on arithmetic operation

---
+++ ArithmeticOperation.py
@@ -0,0 +1,20 @@
+#To perform addition,subtraction,multiplication and division from user's input
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Addition of",a," and ",b,"is ",a+b)
+print("Subtraction of",a,"and",b," is ",a-b)
+print("Multiplication of",a,"and",b," is",a*b)
+print("Quotient and Remainder when ",a,"is divided by ",b,"without decimal point is",a//b,"and",a%b)
+print("Quotient and Remainder when",a,"is divided by",b,"with decimal point is ",a/b,a%b)
+print("The exponent of",a,"and ",b,"is",a**b)
+
+'''
+Enter the first number:5
+Enter the second number:2
+Addition of 5  and  2 is  7
+Subtraction of 5 and 2  is  3
+Multiplication of 5 and 2  is 10
+Quotient and Remainder when  5 is divided by  2 without decimal point is 2 and 1
+Quotient and Remainder when 5 is divided by 2 with decimal point is  2.5 1
+The exponent of 5 and  2 is 25
+'''
