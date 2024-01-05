acca55ae8bd119fad2c7fc2694517119446b4b95
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-05 20:44:44 +0530

    Delete Arithmeticoperation.py

--- Arithmeticoperation.py
+++
@@ -1,7 +0,0 @@
-#To perform addition,subtraction,multiplication and division from user's input
-a=int(input("Enter the first number:"))
-b=int(input("Enter the second number:"))
-print("Addition of ",a," and ",b,"is ",a+b)
-print("Subtraction of",a,"and",b," is ",a-b)
-print("Multiplication of",a,"and",b," is",a*b)
-print("Quotient and Remainder when ",a,"is divided by ",b,"is",a//b,a%b)
