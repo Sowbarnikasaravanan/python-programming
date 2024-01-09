9258b60d7b38414f4755df9aaa8461bca016c329
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-09 21:15:47 +0530

    Example for Relational Operator

---
+++ RelationalOperator.py
@@ -0,0 +1,28 @@
+'''
+Relational Operators
+<    ->Less than
+>    ->Greater than
+<=   ->Less than or equal to
+>=   ->Greater than or equal to
+==   ->equal to
+!=   ->not equal to
+'''
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+'''
+Enter the first number: 10
+Enter the second number:20
+True
+False
+True
+False
+False
+True
+'''
