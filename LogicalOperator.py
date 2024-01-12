3b8465ea64fdcfcce7da543179d1f20a5853fb60
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-12 15:44:14 +0530

    Example for Logical Operators

---
+++ LogicalOperator.py
@@ -0,0 +1,19 @@
+'''
+Logical Operator
+and
+or
+not
+'''
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(a<b and a!=b)
+print(a<b or a!=b)
+print(not a)
+
+'''
+Enter the first number:5
+Enter the second number:10
+True
+True
+False
+'''
