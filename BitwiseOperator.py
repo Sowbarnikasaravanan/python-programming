fa73bdce702f7ba83bb60cfeb7b082a0b27574fa
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-15 16:49:29 +0530

    Example for Bitwise Operator

---
+++ BitwiseOperator.py
@@ -0,0 +1,26 @@
+'''
+Bitwise Operators
+&  ->Bitwise and
+|  ->Bitwise or
+~  ->Bitwise not  
+<< ->Left shift
+>> ->Right shift
+'''
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(a&b)
+print(a|b)
+print(~a)
+print(a<<2)
+print(b>>2)
+
+'''
+Output:
+Enter the first number:5
+Enter the second number:6
+4
+7
+-6
+20
+1
+'''
