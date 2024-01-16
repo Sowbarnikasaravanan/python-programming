6a8abdd6a6a45185f49bfb8b585826c0aa4e386c
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-16 17:48:50 +0530

    Example for identity operator

---
+++ IdentityOperator.py
@@ -0,0 +1,16 @@
+'''
+Identity Operator
+is 
+is not
+'''
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(a is b)
+print(a is not b)
+
+'''
+Enter the first number:5
+Enter the second number:6
+False
+True
+'''
