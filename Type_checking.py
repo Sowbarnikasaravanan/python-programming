c78039f5d987c8534d8dcdbfb6f3c96ff3c19e9b
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-21 15:15:48 +0530

    Example for type checking

---
+++ Type_checking.py
@@ -0,0 +1,34 @@
+#Example for checking the datatype of a variable
+a=12
+b=54.32
+c="apple"
+d=[1,2,3,4]
+e=(5,6,7,8)
+f={1,3,7,9}
+g=(5)
+h=(5,)
+i={a:"sowbarnika",b:"saravanan"}
+j={}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+
+'''
+<class 'int'>
+<class 'float'>
+<class 'str'>
+class 'list'>
+<class 'tuple'>
+<class 'set'>
+<class 'int'>
+<class 'tuple'>
+<class 'dict'>
+<class 'dict'>
+'''
