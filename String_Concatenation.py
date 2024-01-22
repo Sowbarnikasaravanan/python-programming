79443d94116493735e0fa66d865dfe1bcc88922c
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-22 20:48:52 +0530

    Example for string concatenation

---
+++ String_Concatenation.py
@@ -0,0 +1,12 @@
+# Example for string concatenation
+string1=input("Enter the first string:")
+string2=input("Enter the second string:")
+print(string1+string2)
+print(string1+" "+string2)
+
+'''
+Enter the first string:sowbarnika
+Enter the second string:saravanan
+sowbarnikasaravanan
+sowbarnika saravanan
+'''
