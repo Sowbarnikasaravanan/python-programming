027d0c60740e13b29018dbc0d708a02529d49ec3
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-23 20:02:31 +0530

    Example for Dictionary

---
+++ Dictionary_Example.py
@@ -0,0 +1,17 @@
+#Dictionary->is a collection of data in the form of keys-values pair
+#Example for dictionary
+student_data={
+    "Name":"Sowbarnika",
+    "College":"GCE",
+    "rollno":822720104039,
+    "Dept":"CSE"
+}
+#To access the dictonary
+print(student_data)
+# To access a particular value from a dictionary i.e)dept
+print(student_data["Dept"]
+
+'''
+{'Name': 'Sowbarnika', 'College': 'GCE', 'rollno': 822720104039, 'Dept': 'CSE'}
+CSE
+'''
