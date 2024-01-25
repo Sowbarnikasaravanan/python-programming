4b1cf7ffc84963b448198dae312902d9935ef836
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-01-25 21:47:11 +0530

    Example for round function

---
+++ Round_function.py
@@ -0,0 +1,21 @@
+'''
+Round() function
+Syntax:
+    round(number,no_of_digits)
+'''
+#Example:
+value=127.3445345543
+print(round(value,2))
+print(round(value))
+print(round(value,-1))
+print(round(value,-2))
+print(round(value,0))
+
+'''
+Sample output:
+127.34
+127
+130.0
+100.0
+127.0
+'''
