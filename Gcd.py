64c680b17fba19c700d68d200043a5de35e9edda
Sowbarnika S <128996031+Sowbarnikasaravanan@users.noreply.github.com>
2024-02-09 22:59:51 +0530

    To find the gcd of two numbers

--- Gcd.py
+++ Gcd.py
@@ -1,6 +1,6 @@
 #To find the GCD or HCF of two numbers
-num1=int(input("Enter first number:"))
-num2=int(input("Enter second number:"))
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
 if(num1<num2):
     temp=num1
     num1=num2
